cdcbbb57727c876d879690210da726107895fb8b
AMeghnad <anshumanmeghnad@gmail.com>
2017-11-07 11:18:02 +1100

    Initial upload

---
+++ Assets/MicroGames/Anshuman_Meghnad/Game 01/Scripts/RigiEnabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StirlingMulvey;
+
+namespace AnshumanMeghnad
+{
+
+    public class RigiEnabler : MonoBehaviour
+    {
+        Rigidbody myRigi;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRigi = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            myRigi.isKinematic = !GlobalGameManager.gameActive;
+        }
+    }
+}
